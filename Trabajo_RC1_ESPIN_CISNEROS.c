507ff57d1c0d5b191efaaa52fed1aea62de433eb
Christopher-Espin <christopher.espin@udla.edu.ec>
2026-05-31 18:31:04 -0500

    Agregar calificacion maxima y minima por estudiante y por asignatura

--- Trabajo_RC1_ESPIN_CISNEROS.c
+++ Trabajo_RC1_ESPIN_CISNEROS.c
@@ -8,6 +8,8 @@ int main() {
     float suma = 0;
     int i, j;
     int aprobados = 0;
+    float maxEst, minEst;
+    float maxAsig, minAsig;
 
     printf("Ingrese el numero de estudiantes: ");
     scanf("%d", &estudiantes); 
@@ -32,14 +34,49 @@ int main() {
 
         promedio = suma / 3; 
 
+        maxEst = notas[i][0];
+        minEst = notas[i][0];
+        for(j = 1; j < 3; j++) { 
+
+            if(notas[i][j] > maxEst) {
+                maxEst = notas[i][j];
+            }
+
+            if(notas[i][j] < minEst) {
+                minEst = notas[i][j];
+            }
+        }
         printf("Promedio del estudiante: %.2f\n", promedio); 
+        printf("Nota maxima del estudiante: %.2f\n", maxEst);
+        printf("Nota minima del estudiante: %.2f\n", minEst);
 
         if(promedio >= 6)
         {
             aprobados++;
         }
     }
+    printf("\n---Resultados por asignatura---\n");
+    for(j = 0; j < 3; j++) { 
 
+        float sumaAsig = 0;
+        maxAsig = notas[0][j];
+        minAsig = notas[0][j];
+
+        for(i = 0; i < estudiantes; i++) { 
+
+            sumaAsig += notas[i][j];
+
+            if(notas[i][j] > maxAsig) {
+                maxAsig = notas[i][j];
+            }
+
+            if(notas[i][j] < minAsig) {
+                minAsig = notas[i][j];
+            }
+        }
+
+        printf("Asignatura %d - Promedio: %.2f, Max: %.2f, Min: %.2f\n", j + 1, sumaAsig / estudiantes, maxAsig, minAsig);
+    }
     printf("\nTotal aprobados: %d\n", aprobados);
 
     return 0;
